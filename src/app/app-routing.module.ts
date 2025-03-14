592663a8d2170f1d0cf283e0791654dd5da34827
Jhoel <32343423+jhoel0521@users.noreply.github.com>
2025-03-14 00:27:55 +0000

    Agregar componentes de inicio y acerca de, y configurar rutas en el módulo de enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AboutComponent } from './pages/about/about.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'acerca-de', component: AboutComponent },
+  { path: '**', component: InicioComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
