1d7803c1428adbf038075dcd0451d43116e4451f
Jhoel <32343423+jhoel0521@users.noreply.github.com>
2025-03-14 01:13:36 +0000

    comit pro

--- src/app/components/card-producto/card-producto.component.ts
+++ src/app/components/card-producto/card-producto.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { Producto } from '../../models/producto.models';
 @Component({
   selector: 'app-card-producto',
   standalone: false,
   templateUrl: './card-producto.component.html',
   styleUrl: './card-producto.component.css'
 })
-export class CardProductoComponent {
+export class CardProductoComponent implements OnInit {
+  producto: Producto = new Producto();
+  constructor(private route: ActivatedRoute, private _productosService: ProductosService) {
+    this.route.params.subscribe(params => {
+      this.producto = this._productosService.getProducto(parseInt(params['id']) ?? 0);
+    });
+  }
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') || '0';
+    this.producto = this._productosService.getProducto(parseInt(id));
+    console.log(this.producto);
+  }
 
 }
