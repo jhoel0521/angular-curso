f29d6cf605fad91cecf1fd6699b5eab590ab2818
jhoel0521 <jhoel0521@gmail.com>
2025-03-14 11:24:24 -0400

    Agregar componente de búsqueda de productos con visualización de resultados y manejo de parámetros de ruta

--- src/app/components/card-buscador/card-buscador.component.ts
+++ src/app/components/card-buscador/card-buscador.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Producto } from '../../models/producto.models';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
 
 @Component({
   selector: 'app-card-buscador',
@@ -7,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './card-buscador.component.css'
 })
 export class CardBuscadorComponent {
-
+  productosEncontrados: Producto[] = [];
+  termino: string = ''
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _productosService: ProductosService
+  ) { }
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      this.termino = params['termino'];
+      this.productosEncontrados = this._productosService.findProducto(this.termino);
+    });
+  }
 }
