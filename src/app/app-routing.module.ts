805df1148e684b170ee8ac0933b577ae90e137c1
Jhoel <32343423+jhoel0521@users.noreply.github.com>
2025-03-14 00:44:38 +0000

    Agregar rutas para productos, detalle de producto y búsqueda en el módulo de enrutamiento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { CardProductosComponentComponent } from './components/card-productos-component/card-productos-component.component';
+import { CardProductoComponent } from './components/card-producto/card-producto.component';
+import { CardBuscadorComponent } from './components/card-buscador/card-buscador.component';
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
   { path: 'acerca-de', component: AboutComponent },
-  {path: 'productos', component: CardproductosComponent},
+  { path: 'productos', component: CardProductosComponentComponent },
+  { path: 'producto/:id', component: CardProductoComponent },
+  { path: 'buscar/:termino', component: CardBuscadorComponent },
   { path: '**', component: InicioComponent },
 ];
 
