1015c2fbc719fd7af7101806d7bca790ef7f3b25
jhoel0521 <jhoel0521@gmail.com>
2025-03-14 11:09:22 -0400

    Actualizar la barra de navegación para usar routerLink y agregar lógica de ruta activa

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  constructor(private router: Router) { }
 
+  esRutaActiva(ruta: string): boolean {
+    return this.router.url === ruta;
+  }
 }
